6d45e46d62a17f32dc12435e4d72dc577ec73ffd
zhengzy <zhezhiyong@163.com>
2016-10-09 23:51:40 +0800

    fix:163邮件确认测试通过

--- config.py
+++ config.py
@@ -7,13 +7,19 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_USERNAME = 'zhezhiyong@163.com'
+    MAIL_PASSWORD = ''
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    # FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SENDER = 'zhezhiyong@163.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 
 from app import create_app, db
 from app.models import User, Role
@@ -29,4 +29,6 @@ def test():
 
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
+    # manager.add_command("runserver", Server(host="0.0.0.0", port=9000))
+    # manager.run()
