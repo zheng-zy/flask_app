932d7660a3d350e2cab7a44cf967c2af24065a6b
zhengzy <zhezhiyong@163.com>
2016-11-01 10:48:31 +0800

    add:添加管理员级别的用户资料页面功能

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -22,7 +22,8 @@ class RegistrationForm(Form):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('Username', validators=[DataRequired(), Length(1, 64),
                                                    Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-                                                          'Usernames must have only letters, numbers, docs or underscores')])
+                                                          'Usernames must have only letters, '
+                                                          'numbers, docs or underscores')])
     password = PasswordField('Password',
                              validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 from flask import render_template, session, redirect, url_for, current_app, abort
+from flask_login import current_user, flash, login_required
 
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm
 from .. import db
+from ..decorators import admin_required
 from ..email import send_email
-from ..models import User
+from ..models import User, Role
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -39,3 +41,47 @@ def user(username):
     if query_user is None:
         abort(404)
     return render_template('user.html', user=query_user)
+
+
+@main.route('/edit_profile', methods=['GET', 'POST'])
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit_profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
